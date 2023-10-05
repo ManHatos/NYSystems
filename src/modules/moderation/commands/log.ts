b6dc2d7a612a8f5778ab44382d1295af5643fb0f
ManHat <omar.moh.batheeb@gmail.com>
2023-10-05 22:40:09 +0300

    `/log` command action choices

--- src/modules/moderation/commands/log.ts
+++ src/modules/moderation/commands/log.ts
@@ -1,6 +1,7 @@
 import { ApplicationCommandOptionTypes } from "@discordeno/bot";
 import { ModuleCommandElement, ModuleCommandIdentifiers } from "../../modules.js";
-import autocomplete from "../autocomplete/user.js";
+import { RecordActions } from "../../../services/datastore.js";
+import autocomplete1 from "../autocomplete/user.js";
 
 export const id = ModuleCommandIdentifiers.MODERATION_CREATE_NEW;
 export default {
@@ -9,18 +10,25 @@ export default {
 		name: id,
 		description: "Moderate a Roblox user",
 		options: [
-			autocomplete.data,
+			autocomplete1.data,
 			{
 				type: ApplicationCommandOptionTypes.String,
 				name: "reason",
 				description: "The reason for the moderation",
 				required: true,
+				maxLength: 100,
 			},
 			{
 				type: ApplicationCommandOptionTypes.String,
 				name: "action",
 				description: "The action taken on the user",
 				required: true,
+				choices: Object.keys(RecordActions)
+					.filter((key) => isNaN(Number(key)))
+					.map((action) => ({
+						name: action,
+						value: String(RecordActions[action as keyof typeof RecordActions]),
+					})),
 			},
 		],
 	},
