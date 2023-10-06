7f9b5728151ff74ea4d4a4dd702a5ef36d075c75
ManHat <omar.moh.batheeb@gmail.com>
2023-10-06 20:24:10 +0300

    oops..

--- src/modules/moderation/autocomplete/user.ts
+++ src/modules/moderation/autocomplete/user.ts
@@ -11,52 +11,64 @@ export default {
 		type: ApplicationCommandOptionTypes.String,
 		autocomplete: true,
 		name: id,
-		description: "The user's Roblox username or identifier (ID)",
+		description:
+			'The user\'s Roblox username or identifier (ID), use "#" to search using an identifier (ID)',
 		required: true,
 	},
-	async execute(interaction) {
-		const focusedOption = interaction.data?.options?.find((option) => option.focused);
+	async execute(interaction, option) {
+		if (typeof option.value != "string") return;
+		const input = option.value;
 
-		if (String(focusedOption!.value).length < 3)
+		if (input.length < 3 && !input.startsWith("#")) {
 			return await interaction.respond({
 				choices: [],
 			});
+		}
 
 		throttle(["autocomplete", id, String(interaction.user.id)], String(interaction.id), 750)
 			.then(() => {
-				users
-					.search(String(focusedOption!.value))
-					.then(async (response) => {
-						await interaction.respond({
-							choices: response.map((user) => {
-								return {
-									name:
-										limitString(user.displayName, 40) + " (@" + limitString(user.name, 45) + ")",
-									value: String(user.id),
-								};
-							}),
-						});
-					})
-					.catch((e) => {
-						console.log("user search request failed: ", e);
-						users
-							.multiple([String(focusedOption!.value!)])
-							.then(async (response) => {
+				if (input.startsWith("#")) {
+					users
+						.single(Number(input.substring(1)))
+						.then(async (user) => {
+							await interaction.respond({
+								choices: [
+									{
+										name:
+											limitString(user.displayName, 40) + " (@" + limitString(user.name, 45) + ")",
+										value: String(user.id),
+									},
+								],
+							});
+						})
+						.catch(async (e) => {
+							console.log("user specific request failed: ", e);
+							if (
+								typeof e == "string" &&
+								(e.includes("does not exist") || e == "invalid identifier")
+							) {
+								console.log("test success");
 								await interaction.respond({
-									choices: response.map((user) => {
-										return {
-											name:
-												limitString(user.displayName, 40) +
-												" (@" +
-												limitString(user.name, 45) +
-												")",
-											value: String(user.id),
-										};
-									}),
+									choices: [],
 								});
-							})
-							.catch((e) => console.log("users multiple request failed: ", e));
-					});
+							}
+						});
+				} else {
+					users
+						.search(input)
+						.then(async (response) => {
+							await interaction.respond({
+								choices: response.map((user) => {
+									return {
+										name:
+											limitString(user.displayName, 40) + " (@" + limitString(user.name, 45) + ")",
+										value: String(user.id),
+									};
+								}),
+							});
+						})
+						.catch((e) => console.log("user search request failed: ", e));
+				}
 			})
 			.catch(() => console.error("overwritten"));
 	},
