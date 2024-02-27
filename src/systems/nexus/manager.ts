5d53972dac567fb820da5b573aab1f1266e56dc2
ManHat <omar.moh.batheeb@gmail.com>
2024-02-27 03:50:06 +0300

    Nexus manager hotfix

--- src/systems/nexus/manager.ts
+++ src/systems/nexus/manager.ts
@@ -3,5 +3,14 @@ import { SystemManager } from "../types.js";
 // importing and exporting all component files
 import component1 from "./components/getStarted.js";
 import component2 from "./components/selectRegistrar.js";
+import component3 from "./components/confirmAccount.js";
+import component4 from "./components/incorrectAccount.js";
+import component5 from "./components/selectName.js";
 
-export const components: SystemManager["components"] = [component1, component2];
+export const components: SystemManager["components"] = [
+	component1,
+	component2,
+	component3,
+	component4,
+	component5,
+];
